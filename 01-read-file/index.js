95172d45215d0f1d161ebf1a234141aee387cd2b
Olga Zhuravleva <onpol@mail.ru>
2024-01-21 22:54:43 +0300

    feat: feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf-8');
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log(error.message));
